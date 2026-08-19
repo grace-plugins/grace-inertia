44b645ba048b3bfd9140a0fdf331e35c7854be37
Michael Yan <rain@rainboyan.com>
2026-08-19 11:34:18 +0800

    Make InertiaAutoConfiguration ordered before MimeTypesConfiguration
    Closes gh-1

--- plugin/src/main/groovy/org/graceframework/plugins/inertia/InertiaAutoConfiguration.java
+++ plugin/src/main/groovy/org/graceframework/plugins/inertia/InertiaAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 the original author or authors.
+ * Copyright 2024-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,15 @@ package org.graceframework.plugins.inertia;
 
 import javax.servlet.DispatcherType;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.filter.OrderedFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
+import org.grails.plugins.web.mime.MimeTypesConfiguration;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for Inertia Plugin.
@@ -32,7 +34,7 @@ import org.springframework.context.annotation.Configuration;
  * @since 0.1
  */
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration(before = MimeTypesConfiguration.class)
 public class InertiaAutoConfiguration {
 
     @Bean
